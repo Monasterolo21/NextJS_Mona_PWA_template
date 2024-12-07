3ff2d8f610eb742e8b9619a52cd90c636b2b4d58
Samuele Monasterolo <smonasterolo2@gmail.com>
2024-12-07 16:54:56 +0100

    updated resources

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
-      <h1 className="text-5xl font-bold text-primary animate-in fade-in-50">
-        Complete the todo!
+      <h1 className="text-5xl font-bold text-primary animate-in fade-in-50 md:text-6xl lg:text-7xl">
+        Complete the TODOs 🖊️
       </h1>
       <p>Search for the TODO and complete it! 🚀</p>
       <Button variant={"default"} onClick={showToast()}>
